529224194a00ded8f847da6bb9eb26a69a8e59d6
Ilya Gulkov <x@isgulkov.ru>
2019-03-25 07:30:16 +0300

    Implement RSA signature and verification

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -2,6 +2,7 @@
 #include "rsa.hpp"
 
 #include "primes.hpp"
+#include "sha256.h"
 
 namespace isg {
 namespace rsa {
@@ -20,6 +21,16 @@ std::pair<key_pub, key_priv> gen_keypair(const size_t l_mod, const intbig_t& e)
     const intbig_t d = e.inverse_mod(lambda_n);
 
     return { key_pub(e, n), key_priv(d, n) };
+}
+
+std::string calculate_hash(const std::string& msg, hash_sel_t hash)
+{
+    if(hash == SHA256) {
+        return SHA256::sha256_bytes(msg);
+    }
+    else {
+        throw std::logic_error("Unknown hash function `" + std::to_string(hash) + "`");
+    }
 }
 
 key_pub::key_pub(const std::string& e_bytes, const std::string& n_bytes)
@@ -41,6 +52,14 @@ std::string key_pub::encrypt(const std::string& msg) const
     return x_msg.to_string(intbig_t::Base256);
 }
 
+bool key_pub::verify(const std::string& msg, const std::string& sig, hash_sel_t hash) const
+{
+    const std::string v_hash_msg = calculate_hash(msg, hash);
+    const std::string v_hash_sig = encrypt(sig);
+
+    return v_hash_msg == v_hash_sig;
+}
+
 key_priv::key_priv(const std::string& d_bytes, const std::string& n_bytes)
 {
     d = intbig_t::from(d_bytes, intbig_t::Base256);
@@ -58,6 +77,13 @@ std::string key_priv::decrypt(const std::string& msg) const
     x_msg.to_power(d, n);
 
     return x_msg.to_string(intbig_t::Base256);
+}
+
+std::string key_priv::sign(const std::string& msg, hash_sel_t hash) const
+{
+    const std::string v_hash = calculate_hash(msg, hash);
+
+    return decrypt(v_hash);
 }
 
 }
